f80f21a38d8550f1bd6f0dafbf27f266d2c8b8f8
Yannaing Lynn <rghtbossy@gmail.com>
2021-03-13 13:21:51 +0630

    Initial check-in of module Jack The Giant

---
+++ Assets/Scripts/Background Scripts/BGScaler.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BGScaler : MonoBehaviour
+{
+    void Start()
+    {
+        SpriteRenderer sr = GetComponent<SpriteRenderer>();
+        Vector3 temScale = transform.localScale;
+
+        float width = sr.sprite.bounds.size.x;
+
+        float worldHeight = Camera.main.orthographicSize * 2f;
+        float worldWidth = worldHeight / Screen.height * Screen.width;
+
+        temScale.x = worldWidth / width;
+        transform.localScale = temScale;
+    }
+
+}
